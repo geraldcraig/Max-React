a92edc8acfbef4a41fc25b4d2323ba0358286660
geraldcraig <geraldcraig@me.com>
2026-04-28 18:28:29 +0100

    sending http requests to backend working

--- backend/app.js
+++ backend/app.js
@@ -1,27 +1,53 @@
-const bodyParser = require('body-parser');
-const express = require('express');
+import fs from 'node:fs/promises';
 
-const eventRoutes = require('./routes/events');
-const authRoutes = require('./routes/auth');
+import bodyParser from 'body-parser';
+import express from 'express';
 
 const app = express();
 
+app.use(express.static('images'));
 app.use(bodyParser.json());
+
+// CORS
+
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PATCH,DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type,Authorization');
+  res.setHeader('Access-Control-Allow-Origin', '*'); // allow all domains
+  res.setHeader('Access-Control-Allow-Methods', 'GET, PUT');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
   next();
 });
 
-app.use(authRoutes);
+app.get('/places', async (req, res) => {
+  const fileContent = await fs.readFile('./data/places.json');
 
-app.use('/events', eventRoutes);
+  const placesData = JSON.parse(fileContent);
 
-app.use((error, req, res, next) => {
-  const status = error.status || 500;
-  const message = error.message || 'Something went wrong.';
-  res.status(status).json({ message: message });
+  res.status(200).json({ places: placesData });
 });
 
-app.listen(8080);
+app.get('/user-places', async (req, res) => {
+  const fileContent = await fs.readFile('./data/user-places.json');
+
+  const places = JSON.parse(fileContent);
+
+  res.status(200).json({ places });
+});
+
+app.put('/user-places', async (req, res) => {
+  const places = req.body.places;
+
+  await fs.writeFile('./data/user-places.json', JSON.stringify(places));
+
+  res.status(200).json({ message: 'User places updated!' });
+});
+
+// 404
+app.use((req, res, next) => {
+  if (req.method === 'OPTIONS') {
+    return next();
+  }
+  res.status(404).json({ message: '404 - Not Found' });
+});
+
+app.listen(3000);
